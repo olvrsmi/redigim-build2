658cc2c4e129a8027230b5b2899555a2fd50bf96
o <o@dmstfctn.net>
2025-11-20 21:26:15 +0000

    undo final config for build so the dev site works

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-   output: 'export',
-   distDir: 'findings',
-  basePath: '/findings',
+  // output: 'export',
+  // distDir: 'findings',
+  // basePath: '/findings',
 };
 
 export default nextConfig;
